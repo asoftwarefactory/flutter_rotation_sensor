dc363a52a5a75d3d97f09550db8187e36b0e6665
mario <mario.mazzarelli@asfweb.it>
2026-03-10 23:49:57 +0100

    done

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -8,15 +8,15 @@ plugins {
 android {
     namespace = "net.tlserver6y.flutter_rotation_sensor_example"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+     ndkVersion = "29.0.14206865"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
